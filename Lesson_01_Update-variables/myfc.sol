54f462bceb5f7b3f9fa109f0c5bd4e477713dc6b
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-10-18 16:31:30 +0000

    1

--- Lesson_01_Update-variables/myfc.sol
+++ Lesson_01_Update-variables/myfc.sol
@@ -1,26 +1,28 @@
-//SPDX-Licence-Identifier : MIT
-pragma solidity ^0.8.13; // 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-contract myfc{
-    // directly update the variables 
+contract myfc {
+    // Directly initialized variables
     string public hey = "Subrata Das"; 
-    uint256 public num = 10 ;
+    uint256 public num = 10;
 
+    // Variables initialized via constructor
+    string public name;
+    uint256 public no;
 
-    // using construtor 
-
-    string public name ;
-    uint256 public no ;
-    constructor(string memory _name , uint256  _no){
+    // Constructor to set initial values during deployment
+    constructor(string memory _name, uint256 _no) {
         name = _name;
         no = _no;
     }
-    // using Functions
+
+    // Variables to be updated via function
     string public name2;
-    uint256 public num2 ;
-    function addinfo(string memory _name2 , uint256 _num2) public {
+    uint256 public num2;
+
+    // Function to update the variables `name2` and `num2`
+    function addinfo(string memory _name2, uint256 _num2) public {
         name2 = _name2;
-        num2 = _num2 ;
+        num2 = _num2;
     }
-    
 }
